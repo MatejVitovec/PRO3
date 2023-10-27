f305667323b71b4d02d7d98e4768c164889c6838
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2023-10-27 20:22:32 +0200

    optimalizace LSGM - nutno otestovat

--- src/GradientScheme/LeastSquare.cpp
+++ src/GradientScheme/LeastSquare.cpp
@@ -3,9 +3,7 @@
 
 void LeastSquare::init(const Mesh& mesh)
 {
-    Field<Vars<3>> xRowOfM = Field<Vars<3>>(mesh.getCellsSize());
-    Field<Vars<3>> yRowOfM = Field<Vars<3>>(mesh.getCellsSize());
-    Field<Vars<3>> zRowOfM = Field<Vars<3>>(mesh.getCellsSize());
+    Field<std::array<Vars<3>, 3>> M(mesh.getCellsSize());
 
     Field<Vars<3>> delta = Field<Vars<3>>(mesh.getFacesSize());
 
@@ -16,53 +14,42 @@ void LeastSquare::init(const Mesh& mesh)
 
     for (int i = 0; i < cells.size(); i++)
     {
-        xRowOfM[i][0] = 0;
-        xRowOfM[i][1] = 0;
-        xRowOfM[i][2] = 0;
-        yRowOfM[i][0] = 0;
-        yRowOfM[i][1] = 0;
-        yRowOfM[i][2] = 0;
-        zRowOfM[i][0] = 0;
-        zRowOfM[i][1] = 0;
-        zRowOfM[i][2] = 0;
+        for (int k = 0; k < 3; k++)
+        {
+            for (int kk = 0; kk < 3; kk++)
+            {
+                M[i][k][kk] = 0;
+            }            
+        }
 
         for (int j = 0; j < cells[i].ownFaceIndex.size(); j++)
         {
             int idx = cells[i].ownFaceIndex[j];
 
-            xRowOfM[i][0] += delta[idx][0]*delta[idx][0];
-            xRowOfM[i][1] += delta[idx][0]*delta[idx][1];
-            xRowOfM[i][2] += delta[idx][0]*delta[idx][2];
-
-            yRowOfM[i][0] += delta[idx][0]*delta[idx][1];
-            yRowOfM[i][1] += delta[idx][1]*delta[idx][1];
-            yRowOfM[i][2] += delta[idx][1]*delta[idx][2];
-
-            zRowOfM[i][0] += delta[idx][0]*delta[idx][2];
-            zRowOfM[i][1] += delta[idx][1]*delta[idx][2];
-            zRowOfM[i][2] += delta[idx][2]*delta[idx][2];
+            for (int k = 0; k < 3; k++)
+            {
+                for (int kk = 0; kk < 3; kk++)
+                {
+                    M[i][k][kk] += delta[idx][k]*delta[idx][kk];
+                }                
+            }
         }
 
         for (int j = 0; j < cells[i].neighborFaceIndex.size(); j++)
         {
             int idx = cells[i].neighborFaceIndex[j];
 
-            xRowOfM[i][0] += (-delta[idx][0])*(-delta[idx][0]);
-            xRowOfM[i][1] += (-delta[idx][0])*(-delta[idx][1]);
-            xRowOfM[i][2] += (-delta[idx][0])*(-delta[idx][2]);
-
-            yRowOfM[i][0] += (-delta[idx][0])*(-delta[idx][1]);
-            yRowOfM[i][1] += (-delta[idx][1])*(-delta[idx][1]);
-            yRowOfM[i][2] += (-delta[idx][1])*(-delta[idx][2]);
-
-            zRowOfM[i][0] += (-delta[idx][0])*(-delta[idx][2]);
-            zRowOfM[i][1] += (-delta[idx][1])*(-delta[idx][2]);
-            zRowOfM[i][2] += (-delta[idx][2])*(-delta[idx][2]);
+            for (int k = 0; k < 3; k++)
+            {
+                for (int kk = 0; kk < 3; kk++)
+                {
+                    M[i][k][kk] -= delta[idx][k]*delta[idx][kk];
+                }                
+            }
         }
     }
 
-    calculateInverseM(xRowOfM, yRowOfM, zRowOfM);
-
+    calculateInverseM(M);
 }
 
 
@@ -91,43 +78,38 @@ void LeastSquare::calculatesDeltas(const Mesh& mesh)
 }
 
 
-void LeastSquare::calculateInverseM(Field<Vars<3>> xRowOfM, Field<Vars<3>> yRowOfM, Field<Vars<3>> zRowOfM)
+void LeastSquare::calculateInverseM(Field<std::array<Vars<3>, 3>> M)
 {
-    for (int i = 0; i < xRowOfMInv.size(); i++)
+    for (int i = 0; i < MInv.size(); i++)
     {
-        double det = det3by3(xRowOfM[i], yRowOfM[i], zRowOfM[i]);
-        std::array<double, 9> adj = adjoint3by3(xRowOfM[i], yRowOfM[i], zRowOfM[i]);
+        double det = det3by3(M[i]);
+        std::array<Vars<3>, 3> adj = adjoint3by3(M[i]);
 
-        xRowOfMInv[i][0] = adj[0]/det;
-        xRowOfMInv[i][1] = -adj[1]/det;
-        xRowOfMInv[i][2] = adj[2]/det;
-        yRowOfMInv[i][0] = -adj[3]/det;
-        yRowOfMInv[i][1] = adj[4]/det;
-        yRowOfMInv[i][2] = -adj[5]/det;
-        zRowOfMInv[i][0] = adj[6]/det;
-        zRowOfMInv[i][1] = -adj[7]/det;
-        zRowOfMInv[i][2] = adj[8]/det;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if(j*k % 2 == 0)
+                    MInv[i][j][k] = adj[j][k]/det;
+                else
+                    MInv[i][j][k] = -adj[j][k]/det;                
+            }            
+        }
     }
 }
 
 
-double LeastSquare::det3by3(Vars<3> xRow, Vars<3> yRow, Vars<3> zRow) const
+double LeastSquare::det3by3(std::array<Vars<3>, 3> M) const
 {
-    return xRow[0]*yRow[1]*zRow[2] + xRow[1]*yRow[2]*zRow[0] + xRow[2]*yRow[0]*zRow[1] - xRow[0]*yRow[2]*zRow[1] - xRow[1]*yRow[0]*zRow[2] - xRow[2]*yRow[1]*zRow[0];
+    return M[0][0]*M[1][1]*M[2][2] + M[0][1]*M[1][2]*M[2][0] + M[0][2]*M[1][0]*M[2][1] - M[0][0]*M[1][2]*M[2][1] - M[0][1]*M[1][0]*M[2][2] - M[0][2]*M[1][1]*M[2][0];
 }
 
 
-std::array<double, 9> LeastSquare::adjoint3by3(Vars<3> xRow, Vars<3> yRow, Vars<3> zRow) const
+std::array<Vars<3>, 3> LeastSquare::adjoint3by3(std::array<Vars<3>, 3> M) const
 {
-    return {yRow[1]*zRow[2] - yRow[2]*zRow[1],
-            yRow[0]*zRow[2] - yRow[2]*zRow[0],
-            yRow[0]*zRow[1] - yRow[1]*zRow[0],
-            xRow[1]*zRow[2] - xRow[2]*zRow[1],
-            xRow[0]*zRow[2] - xRow[2]*zRow[0],
-            xRow[0]*zRow[1] - xRow[1]*zRow[0],
-            xRow[1]*yRow[2] - xRow[2]*yRow[1],
-            xRow[0]*yRow[2] - xRow[2]*yRow[0],
-            xRow[0]*yRow[1] - xRow[1]*yRow[0]};
+    return {Vars<3>({M[1][1]*M[2][2] - M[1][2]*M[2][1], M[1][0]*M[2][2] - M[1][2]*M[2][0], M[1][0]*M[2][1] - M[1][1]*M[2][0]}),
+            Vars<3>({M[0][1]*M[2][2] - M[0][2]*M[2][1], M[0][0]*M[2][2] - M[0][2]*M[2][0], M[0][0]*M[2][1] - M[0][1]*M[2][0]}),
+            Vars<3>({M[0][1]*M[1][2] - M[0][2]*M[1][1], M[0][0]*M[1][2] - M[0][2]*M[1][0], M[0][0]*M[1][1] - M[0][1]*M[1][0]})};
 }
 
 
@@ -155,45 +137,34 @@ Field<std::array<Vars<5>, 3>> LeastSquare::calculateGradient(const Field<Compres
         {
             int idx = cells[i].ownFaceIndex[j];
 
-            b[i][0] += (wr[idx][0] - wl[idx][0])*delta[idx];
-            b[i][1] += (wr[idx][1] - wl[idx][1])*delta[idx];
-            b[i][2] += (wr[idx][2] - wl[idx][2])*delta[idx];
-            b[i][3] += (wr[idx][3] - wl[idx][3])*delta[idx];
-            b[i][4] += (wr[idx][4] - wl[idx][4])*delta[idx];
+            for (int k = 0; k < 5; k++)
+            {
+                b[i][k] += (wr[idx][k] - wl[idx][k])*delta[idx];
+            }
         }
 
         for (int j = 0; j < cells[i].neighborFaceIndex.size(); j++)
         {
             int idx = cells[i].neighborFaceIndex[j];
 
-            b[i][0] += (wl[idx][0] - wr[idx][0])*delta[idx];
-            b[i][1] += (wl[idx][1] - wr[idx][1])*delta[idx];
-            b[i][2] += (wl[idx][2] - wr[idx][2])*delta[idx];
-            b[i][3] += (wl[idx][3] - wr[idx][3])*delta[idx];
-            b[i][4] += (wl[idx][4] - wr[idx][4])*delta[idx];
+            for (int k = 0; k < 5; k++)
+            {
+                b[i][k] += (wl[idx][k] - wr[idx][k])*delta[idx];
+            }
         }
     }
 
     for (int i = 0; i < cells.size(); i++)
     {
-        Vars<5> aux;
-        for (int j = 0; j < 5; j++)
-        {            
-            aux[j] = dot(xRowOfMInv[i], b[i][j]);
-        }
-        grad[i][0] = aux;
-
-        for (int j = 0; j < 5; j++)
-        {            
-            aux[j] = dot(yRowOfMInv[i], b[i][j]);
-        }
-        grad[i][1] = aux;
-
-        for (int j = 0; j < 5; j++)
-        {            
-            aux[j] = dot(yRowOfMInv[i], b[i][j]);
-        }
-        grad[i][2] = aux;        
+        for (int k = 0; k < 3; k++)
+        {
+            Vars<5> aux;
+            for (int j = 0; j < 5; j++)
+            {            
+                aux[j] = dot(MInv[i][k], b[i][j]);
+            }
+            grad[i][k] = aux;
+        }     
     }
 
     return grad;
--- src/GradientScheme/LeastSquare.hpp
+++ src/GradientScheme/LeastSquare.hpp
@@ -16,17 +16,15 @@ class LeastSquare : public GradientScheme
         Field<std::array<Vars<5>, 3>> calculateGradient(const Field<Compressible>& wl, const Field<Compressible>& wr, const Mesh& mesh) const;
 
     private:
-        Field<Vars<3>> xRowOfMInv;
-        Field<Vars<3>> yRowOfMInv;
-        Field<Vars<3>> zRowOfMInv;
+        Field<std::array<Vars<3>, 3>> MInv;
 
         Field<Vars<3>> delta;
 
         void calculatesDeltas(const Mesh& mesh);
 
-        void calculateInverseM(Field<Vars<3>> xRowOfM, Field<Vars<3>> yRowOfM, Field<Vars<3>> zRowOfM);        
-        double det3by3(Vars<3> xRow, Vars<3> yRow, Vars<3> zRow) const;
-        std::array<double, 9> adjoint3by3(Vars<3> xRow, Vars<3> yRow, Vars<3> zRow) const;
+        void calculateInverseM(Field<std::array<Vars<3>, 3>> M);        
+        double det3by3(std::array<Vars<3>, 3> M) const;
+        std::array<Vars<3>, 3> adjoint3by3(std::array<Vars<3>, 3> M) const;
 
 };
 
